93387428e6dfb5d33a68188c1ad7bcce612406f7
shaglama <shaglama@gmail.com>
2018-01-02 05:49:25 -0500

    Add files via upload
    changed > operator to >! operator when writing to pid file to ensure it overwrites even if noclobber is set

--- installNode.sh
+++ installNode.sh
@@ -344,7 +344,7 @@ echo "echo 'to detach from node while attached : hold control and press a. press
 echo "echo 'to kill node while attached: hold control and press a. press k. press y.'" >> $STRT
 #echo "touch '/home/$HEAT_USER/HeatLedger/startHeatLedger.pid"
 echo "screen -dmS heatLedger /bin/bash $BIN &" >> $STRT
-echo "screen -list | grep 'heatLedger' | cut -f1 -d'.' | sed 's/\W//g' > '/home/$HEAT_USER/HeatLedger/startHeatLedger.pid'" >> $STRT
+echo "screen -list | grep 'heatLedger' | cut -f1 -d'.' | sed 's/\W//g' >! '/home/$HEAT_USER/HeatLedger/startHeatLedger.pid'" >> $STRT #use >! to ignore noclobber
 sudo chmod +x $STRT
 
 #create mining start script
